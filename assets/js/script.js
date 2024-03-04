bfdd34da64ec65e5331e869bdb0709423bb10c25
englburc <76907930+englburc@users.noreply.github.com>
2024-03-04 10:26:37 +0100

    inserted timeslots

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,24 @@ const elDivTimeblocks = $(".container")
 
 const timeSlots = [["8AM", 8], ["9AM", 9], ["10AM", 10], ["11AM", 11],
 ["12PM", 12], ["1PM", 13], ["2PM", 14], ["3PM", 15], ["4PM", 16], ["5PM", 17]];
+
 // Display the current day at the top of the calender when a user opens the planner.
 elPCurrentDay.text(dayjs().format("dddd, MMMM D"))
 
 
 // Present time blocks for standard business hours when the user scrolls down.
+$.each(timeSlots, function(i, t){
+    let row = $("<div class=´row´></div>");
+    let hour = $("<div class=´hour´></div>")
+    let description = $("<textarea class=´description´></textarea>")
+    let button = $("<div class=´button´></div>")
 
+    hour.html("<br>" + t[0]);
+    row.attr("data-time", t[1]);
+    row.append(hour, description, button);
+    elDivTimeblocks.append(row);
+
+})
 // Color-code each time block based on past, present, and future when the time block is viewed.
 
 // Allow a user to enter an event when they click a time block
