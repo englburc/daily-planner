41bb58963c135f21d30f1a6034a423169523e19e
englburc <teacher.levan@gmail.com>
2024-03-10 19:24:44 +0000

    onlick button action to save in localStarage and get tasks, also guard from null localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,11 @@ elPCurrentDay.text(dayjs().format("dddd, MMMM D"))
 
 
 // Present time blocks for standard business hours when the user scrolls down.
-$.each(timeSlots, function(i, t){
+$.each(timeSlots, function (i, t) {
     let row = $("<div>").addClass("row");
-    let hour = $("<div>").addClass("hour");
-    let description = $("<textarea>").addClass("description");
-    let button = $("<div>").addClass("button");
+    let hour = $("<div>").addClass("hour col-sm-2 col-lg-1");
+    let description = $("<textarea>").addClass("description col-sm col-lg");
+    let button = $("<div>").addClass("button col-sm-2 col-lg-1");
 
     hour.html("<br>" + t[0]);
     row.attr("data-time", t[1]);
@@ -24,7 +24,29 @@ $.each(timeSlots, function(i, t){
 // Color-code each time block based on past, present, and future when the time block is viewed.
 
 // Allow a user to enter an event when they click a time block
-
-// Save the event in local storage when the save button is clicked in that time block.
+elDivTimeblocks.on('click', '.button', function (e) {
+    e.preventDefault();
+    const tasksList = JSON.parse(localStorage.getItem("tasksList"));
+    if (tasksList == null) {
+        let tasks = {
+            "8": "",
+            "9": "",
+            "10": "",
+            "11": "",
+            "12": "",
+            "13 ": "",
+            "14": "",
+            "15": "",
+            "16": "",
+            "17": ""
+        }
+        localStorage.setItem("tasksList", JSON.stringify(tasks));
+    } else {
+        let key = $(e.target).parent().attr("data-time").toString();
+        tasksList[key] = $(e.target).siblings('.description').val() || "";
+        // Save the event in local storage when the save button is clicked in that time block.
+        localStorage.setItem("tasksList", JSON.stringify(tasksList));
+    }
+})
 
 // Persist events between refreshes of a page
